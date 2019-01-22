540698bfc962676ae84a8f414491be385da5c1ff
dimka048 <dimka048@gmail.com>
2019-01-22 20:41:37 +0300

    From Work

---
+++ PainCsharp/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace PainCsharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBarConvertToTxt = new System.Windows.Forms.ProgressBar();
+            this.progressConvertToTxt = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Выбрать первое изображение";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 51);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(357, 51);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(357, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(177, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Выбрать второе изображение";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog1";
+            this.openFileDialog2.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog2_FileOk);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(669, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(187, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Построить гисторгаммы";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(669, 51);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(187, 37);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Сохранить изменённое изображение";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox3.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.pictureBox3.Location = new System.Drawing.Point(12, 409);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(300, 225);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox4.Location = new System.Drawing.Point(357, 409);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(300, 225);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 8;
+            this.pictureBox4.TabStop = false;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(669, 107);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(186, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Сохранить в txt изображение №";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.ImToTxt_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2"});
+            this.comboBox1.Location = new System.Drawing.Point(861, 109);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(36, 21);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 354);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Разрешение изображения:";
+            // 
+            // progressBarConvertToTxt
+            // 
+            this.progressBarConvertToTxt.Location = new System.Drawing.Point(663, 686);
+            this.progressBarConvertToTxt.Name = "progressBarConvertToTxt";
+            this.progressBarConvertToTxt.Size = new System.Drawing.Size(301, 23);
+            this.progressBarConvertToTxt.TabIndex = 12;
+            this.progressBarConvertToTxt.Visible = false;
+            // 
+            // progressConvertToTxt
+            // 
+            this.progressConvertToTxt.AutoSize = true;
+            this.progressConvertToTxt.Location = new System.Drawing.Point(660, 670);
+            this.progressConvertToTxt.Name = "progressConvertToTxt";
+            this.progressConvertToTxt.Size = new System.Drawing.Size(298, 13);
+            this.progressConvertToTxt.TabIndex = 13;
+            this.progressConvertToTxt.Text = "Выполняется преобразование изображения в формат txt";
+            this.progressConvertToTxt.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(354, 354);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Разрешение изображения:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 393);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(190, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Гистограмма первого изображения";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(354, 393);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(189, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Гистограмма второго изображения";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(967, 713);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressConvertToTxt);
+            this.Controls.Add(this.progressBarConvertToTxt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBarConvertToTxt;
+        private System.Windows.Forms.Label progressConvertToTxt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ PainCsharp/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PainCsharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public Image Histogramma(Image PictureOne)
+        {
+            Bitmap barChart = null;
+            Bitmap PictureOne1 = new Bitmap(PictureOne);
+            barChart = new Bitmap(PictureOne.Width, PictureOne.Height);
+            int[] R = new int[256];
+            int[] G = new int[256];
+            int[] B = new int[256];
+            int i, j;
+            Color color;
+            for (i = 0; i < PictureOne.Width; ++i)
+                for (j = 0; j < PictureOne.Height; ++j)
+                {
+                    color = PictureOne1.GetPixel(i, j);
+                    ++R[color.R];
+                    ++G[color.G];
+                    ++B[color.B];
+                }
+            int max = 0;
+            for (i = 0; i < 256; ++i)
+            {
+                if (R[i] > max)
+                    max = R[i];
+                if (G[i] > max)
+                    max = G[i];
+                if (B[i] > max)
+                    max = B[i];
+            }
+            double point = (double)max / PictureOne.Height;
+            for (i = 0; i < PictureOne.Width - 3; ++i)
+            {
+                for (j = PictureOne.Height - 1;
+                    j > PictureOne.Height - R[i / 4] / point;
+                    --j)
+                {
+                    barChart.SetPixel(i, j, Color.Red);
+                }
+                ++i;
+                for (j = PictureOne.Height - 1; j > PictureOne.Height - G[i / 4] / point; --j)
+                {
+                    barChart.SetPixel(i, j, Color.Green);
+                }
+                ++i;
+                for (j = PictureOne.Height - 1; j > PictureOne.Height - B[i / 4] / point; --j)
+                {
+                    barChart.SetPixel(i, j, Color.Blue);
+                }
+            }
+            return barChart;
+        }
+
+        public void FromImageToTxt(Image image)
+        {
+            progressConvertToTxt.Visible = true;
+            progressBarConvertToTxt.Visible = true;
+            //Объявляем переменную для формирования строки первой матрицы в текстовом представлении (значения по оси Х картинки)
+            string FileLine1 = string.Empty;
+            //Объявляем список строк, в который будем построчно добавлять матрицу в текстовом виде
+            List<string> file1 = new List<string>();
+
+            //Объявляем переменную для формирования строки матрицы, в которую будем писать значения цветов в формате ARGB
+            string FileLine2 = string.Empty;
+            //Список строк для формирования второго файла
+            List<string> file2 = new List<string>();
+
+            Bitmap b1 = new Bitmap(image);
+
+            //Объявляем переменные для значений высоты и ширины матрицы (картинки)...
+            //...и тут же задаем значения этих переменных взяв их из высоты и ширины картинки в пикселях
+            int height = b1.Height; //Это высота картинки, и наша матрица по вертикали будет состоять из точно такого же числа элементов.
+            int width = b1.Width; //Это ширина картинки, т.е. число элементов матрицы по горизонтали
+            progressBarConvertToTxt.Maximum = height+2;
+            //Тут мы объявляем саму матрицу в виде двумерного массива,
+            Color[,] colorMatrix = new Color[width, height];
+
+            //Цикл будет выполняться от 0 и до тех пор, пока y меньше height (высоты матрицы и картинки)
+            //На каждой итерации увеличиваем значение y на единицу.
+            for (int y = 0; y < height; y++)
+            {
+                //В начале каждой итерации мы обнуляем переменные для формирования строк для файлов
+                FileLine1 = string.Empty;
+                FileLine2 = string.Empty;
+                //А теперь сканируем горизонтальные строки матрицы:
+                for (int x = 0; x < width; x++)
+                {
+                    //В матрицу добавляем цвет точки с координатами x,y из картинки b1.            
+                    colorMatrix[x, y] = b1.GetPixel(x, y);
+                    //А теперь преобразуем цвет точки (x,y) в:
+                    //1. Текстовое представление:
+                    FileLine1 += colorMatrix[x, y].ToString() + " ";
+                    //2. Значение цвета в целочисленном формате:
+                    FileLine2 += colorMatrix[x, y].ToArgb().ToString() + " ";
+                    //Тут в обоих случаях в конечном итоге строки преобразуются к типу string для удобства их сохранения в текстовом файле
+                }
+                //А теперь в списки добавляем каждую из полученных строк:
+                //Строка картинки в текстовом виде:
+                file1.Add(FileLine1);
+                //Строка картинки в виде значения цветов пикселей
+                file2.Add(FileLine2);
+                progressBarConvertToTxt.Value++;
+            }
+            //Записываем полученные результаты в текстовые файлы:
+            File.WriteAllLines("text.txt", file1);
+            progressBarConvertToTxt.Value++;
+            File.WriteAllLines("ARGB.txt", file2);
+            progressBarConvertToTxt.Value++;
+            MessageBox.Show("Изображение сохранено");
+            progressBarConvertToTxt.Visible = false;
+            progressConvertToTxt.Visible = false;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                System.IO.StreamReader sr = new
+                   System.IO.StreamReader(openFileDialog1.FileName);
+                sr.Close();
+                label1.Text = label1.Text + pictureBox1.Image.Width.ToString() + "x" + pictureBox1.Image.Height.ToString();
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            try
+            {
+                pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+            }
+            catch(OutOfMemoryException mem)
+            {
+                MessageBox.Show("Что-то с памятью моей стало \r\n" + mem.ToString());
+            }
+            catch(ArgumentException arg)
+            {
+                MessageBox.Show("Что-то не то пошло на вход \r\n" + arg.ToString());
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if (openFileDialog2.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                StreamReader sr = new
+                  StreamReader(openFileDialog2.FileName);
+                sr.Close();
+                label2.Text = label2.Text + pictureBox2.Image.Width.ToString() + "x" + pictureBox2.Image.Height.ToString();
+            }
+        }
+
+        private void openFileDialog2_FileOk(object sender, CancelEventArgs e)
+        {
+            try
+            {
+                pictureBox2.Image = Image.FromFile(openFileDialog2.FileName);
+            }
+            catch (OutOfMemoryException mem)
+            {
+                MessageBox.Show("Что-то с памятью моей стало \r\n" + mem.ToString());
+            }
+            catch (ArgumentException arg)
+            {
+                MessageBox.Show("Что-то не то пошло на вход \r\n" + arg.ToString());
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //label1.Text = label1.Text + pictureBox1.Image.Width.ToString() + "x" + pictureBox1.Image.Height.ToString();
+            Bitmap PictureOne = new Bitmap(pictureBox1.Image); // это будем менять
+            Bitmap PictureTwo = new Bitmap(pictureBox2.Image); // это изображение останется неизменным
+            //for (int y = 0; y < PictureOne.Height; y++)
+            //    for (int x = 0; x < PictureOne.Width; x++)
+            //    {
+            //        var NewR = PictureOne.GetPixel(x, y).R;
+            //        var NewG = PictureOne.GetPixel(x, y).G;
+            //        var NewB = PictureOne.GetPixel(x, y).B;
+            //      //if (NewR > 240)
+            //      //      NewR = 240;
+            //      //  else if (NewG > 240)
+            //      //      NewG = 100;
+            //      //  else if (NewB > 240)
+            //      //      NewB = 10;
+            //            PictureOne.SetPixel(x, y, Color.FromArgb(NewR, NewG, NewB));
+            //    }
+           // pictureBox2.Image = PictureOne;
+            // пробуем создать гистограмму
+            Bitmap barChart = null;
+            Bitmap barChart2 = null;
+            barChart = new Bitmap(Histogramma(PictureOne));
+            barChart2 = new Bitmap(Histogramma(PictureTwo));
+            pictureBox3.Image = barChart;
+            pictureBox4.Image = barChart2;
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "Image Files(*.BMP)|*.BMP|Image Files(*.JPG)|*.JPG|Image Files(*.GIF)|*.GIF|Image Files(*.PNG)|*.PNG|All files (*.*)|*.*";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox2.Image.Save(saveFileDialog1.FileName);
+            }
+            MessageBox.Show("Файл сохранен");
+        }
+
+
+        private void ImToTxt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int number = int.Parse(comboBox1.Text);
+                switch (number)
+                {
+                    case 1:
+                        FromImageToTxt(pictureBox1.Image);
+                        break;
+                    case 2:
+                        FromImageToTxt(pictureBox2.Image);
+                        break;
+                    default:
+                        MessageBox.Show("Что-то не так с номером изображения, которое вы хотите преобразовать в txt формат");
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Хэй, что-то тут не так с номером или преобразованием\n" + ex.ToString());
+            }
+        }
+    }
+}
